ed7af37b4bf6789c92063f56937208eda7c1bc46
awarn <anton@rebelandbird.com>
2017-11-16 14:43:22 +0100

    Add GA Ecommerce tracking code

---
+++ dataLayer-addToCart.js
@@ -0,0 +1,17 @@
+// Measure adding a product to a shopping cart by using an 'add' actionFieldObject
+// and a list of productFieldObjects.
+dataLayer.push({
+  'event': 'addToCart',
+  'ecommerce': {
+    'currencyCode': 'SEK',
+    'add': {                                // 'add' actionFieldObject measures.
+      'products': [{                        //  adding a product to a shopping cart.
+        'name': 'Lotten',
+        'id': 'ticket-subscription-001',
+        'price': '160',											// update if price changes
+        'category': 'Ticket',
+        'quantity': INTEGER
+       }]
+    }
+  }
+});
---
+++ dataLayer-checkout.js
@@ -0,0 +1,27 @@
+// 
+dataLayer.push({
+	'event': 'checkout',
+	'ecommerce': {
+		'checkout': {
+			'actionField': {'step': 1, 'option': 'AG BankId'},
+			'products': [{
+				'name': 'Lotten',												// Name or ID is required.
+        'id': 'ticket-subscription-001',
+        'price': '160',													// Update if price changes
+        'category': 'Ticket',
+        'quantity': INTEGER											
+			}]
+		}
+  }
+})
+
+// Alternative way of adding information about the checkout
+// Use this after sending the `'event': 'checkout'` (the above).
+dataLayer.push({
+	'event': 'checkoutOption',
+	'ecommerce': {
+		'checkout_option': {
+			'actionField': {'step': 1, 'option': 'AG Blankett'}
+		}
+	}
+});
